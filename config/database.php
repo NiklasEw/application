94ab3c27bfd23b79b57fcf35e3d54c022dd886be
Niklas Rasmus Ewes <niklas.ewes@AD.BIELEFELD-GYM-MAPL.LOGODIDACT.NET>
2020-11-17 14:39:34 +0100

    20201117

--- config/database.php
+++ config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'D:\usbwebserver\CI3\assets\db\database.db',
 	'hostname' => 'localhost',
 	'username' => '',
 	'password' => '',
 	'database' => '',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
